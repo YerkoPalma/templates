a9f834a939ebbbcf793d47afe825b2d91585109d
Yerko Palma <yerko.palma@usach.cl>
2017-06-22 19:43:19 +0000

    improved resources in full-stack template

--- full-stack/api/index.js
+++ full-stack/api/index.js
@@ -7,21 +7,13 @@ var Nanostack = require('nanostack')
 var app = merry()
 var stack = Nanostack()
 // push to middleware
-stack.push(function timeElapsed (ctx, next) {
-  var start = Date.now()
 
-  next(null, function (err, ctx, next) {
-    if (err) return next(err)
-    var now = Date.now()
-    var elapsed = start - now
-    console.log('time elapsed: ' + elapsed + 'ms')
-    next()
-  })
-})
 var resource = Resource(app, stack)
+
 var db = process.env.ENV !== 'production'
         ? require('memdb')() : level(process.env.DB)
 var Post = Model(db, 'post')
+
 var opt = {
   version: 1,
   path: 'post'
@@ -30,7 +22,7 @@ var opt = {
 resource(Post, opt)
 
 app.route('default', function (req, res, ctx) {
-  ctx.send(404, { message: 'nada butts here' })
+  ctx.send(404, { message: 'not found' })
 })
 
 module.exports = app
--- full-stack/tests/api.spec.js
+++ full-stack/tests/api.spec.js
@@ -22,11 +22,12 @@ tape('/api/v1/post', function (t) {
     })
   })
 
-  t.test('GET', function (assert) {
-    assert.plan(1)
+  t.test('GET and middleware', function (assert) {
+    assert.plan(2)
 
     makeRequest('GET', '/api/v1/post', null, function (body, res) {
       assert.equal(res.statusCode, 200)
+      assert.equal(res.headers['awesome-header'], 'Header set')
     })
   })
 
@@ -43,6 +44,49 @@ tape('/api/v1/post', function (t) {
     })
   })
 
+  t.test('PUT', function (assert) {
+    assert.plan(4)
+    var post = { title: 'Foo bar wow', content: 'lorem ipsum' }
+
+    makeRequest('POST', '/api/v1/post', post, function (body, res) {
+      post.title = 'New Foo Title'
+      assert.equal('Foo bar wow', body.data.title)
+      makeRequest('PUT', '/api/v1/post/' + body.data.id, post, function (body, res) {
+        assert.equal(res.statusCode, 200)
+        assert.equal('New Foo Title', body.data.title)
+        assert.equal('lorem ipsum', body.data.content)
+      })
+    })
+  })
+
+  t.test('DELETE', function (assert) {
+    assert.plan(3)
+    var post = { title: 'Foo bar wow', content: 'lorem ipsum' }
+
+    makeRequest('POST', '/api/v1/post', post, function (body, res) {
+      assert.equal(res.statusCode, 200)
+      var id = body.data.id
+      makeRequest('DELETE', '/api/v1/post/' + id, null, function (body, res) {
+        assert.equal(res.statusCode, 200)
+        makeRequest('GET', '/api/v1/post/' + id, null, function (body, res) {
+          assert.equal(res.statusCode, 404)
+        })
+      })
+    })
+  })
+
+  t.test('overwrite', function (assert) {
+    assert.end()
+  })
+
+  t.test('middleware can cancel request', function (assert) {
+    makeRequest('GET', '/api/v1/post/fake', null, function (body, res) {
+      assert.equal(res.statusCode, 500)
+      assert.equal(body.message, 'What are you doing?')
+      t.end()
+    })
+  })
+
   function makeRequest (method, route, data, cb) {
     var req = http.request({ port: 8080, method: method, path: route, headers: {'Content-Type': 'application/json'} }, function (res) {
       res.on('error', function (err) {
@@ -52,7 +96,10 @@ tape('/api/v1/post', function (t) {
       res.on('data', function (chunk) {
         body.push(chunk)
       })
-      res.on('end', function () { cb(JSON.parse(body.toString()), res) })
+      res.on('end', function () {
+        var bodyString = body.toString()
+        cb(bodyString ? JSON.parse(bodyString) : '{}', res)
+      })
     })
     req.on('error', function (err) {
       t.error(err)
